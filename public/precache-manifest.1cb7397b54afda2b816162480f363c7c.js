1163a7ea9956914060763c0eab78afeb5ba910e6
caprosset <capucinerosset@gmail.com>
2019-12-19 01:15:19 +0100

    Production v.3.0.1

---
+++ public/precache-manifest.1cb7397b54afda2b816162480f363c7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed28b29fbaadf71178cd4bd8f831aff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f77f823bfa7a58a2be02",
+    "url": "/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "8ab42af4e7a8c1f4b277",
+    "url": "/static/css/main.c1fcb00b.chunk.css"
+  },
+  {
+    "revision": "f77f823bfa7a58a2be02",
+    "url": "/static/js/2.1d864347.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.1d864347.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ab42af4e7a8c1f4b277",
+    "url": "/static/js/main.c1cab9bc.chunk.js"
+  },
+  {
+    "revision": "5bba74af4a4e52b458f9",
+    "url": "/static/js/runtime-main.e8ac4ea3.js"
+  },
+  {
+    "revision": "6143ca787bc28e8c5c37815fec923bca",
+    "url": "/static/media/ironhack.6143ca78.png"
+  }
+]);
